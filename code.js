f4babd55d1b0e58c85ce9b391d31e8db94f6b4bd
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-10-28 15:33:46 +0000

    Added the main image, started to add and position items, added wrong touch detection and correct item box disappearance

--- code.js
+++ code.js
@@ -18,12 +18,20 @@ function reduceTime() {
 }
 
 document.addEventListener('click', (i) => {
-    if (i.target.id === 'score') {
-        alert("image found");
+    if (i.target.id === 'lamp') {
+        timeleft=timeleft+2;
         boxes[0]?.remove();
+        document.getElementById("lamp").style.display="none";
         numscore=numscore+1;
         score.textContent = "Items Found: "+numscore; 
+    } else if (i.target.id === 'keys') {
+        timeleft=timeleft+2;
+        boxes[1]?.remove();
+        numscore=numscore+1;
+        score.textContent = "Items Found: "+numscore; 
+    } else if (i.target.id === 'image1') {
+        alert("NOT AN ITEM!")
     }
 });
 
-setInterval(reduceTime,1000);
+//setInterval(reduceTime,1000);
