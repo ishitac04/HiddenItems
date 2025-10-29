ccfb71317b77473f74b7e3696dfe347f2725ebbe
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-10-29 21:58:57 +0000

    added the images for random boxes and created CSS elements for them

--- infinitecode.js
+++ infinitecode.js
@@ -1,4 +1,4 @@
-let array = ["sunglasses", "cup", "two", "three", "four", "five", "six", "seven"];
+let array = ["umbrella","shell","flower","dice","phone","extinguisher","basketball","cone","target","books","shoes","pencil","bottle","comb","perfume","teddy","fish","cake","crystal","megaphone"];
 let useditems=[];
 let index;
 let index2;
@@ -24,7 +24,7 @@ function addItems() {
 }
 
 function addItems2() {
-    for (let n=0; n < 5; n++) {
+    for (let n=0; n < 15; n++) {
         index2 = Math.floor(Math.random()*(array.length));
         while (useditems.includes(array[index2])) {
             index2 = Math.floor(Math.random()*(array.length));
