12737892a742a71d68e00014e9b25105b642d118
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-10-29 18:00:56 +0000

    finalised the grid, added random item adding to the grid, and made sure that two boxes don't have the same image

--- infinitecode.js
+++ infinitecode.js
@@ -1,12 +1,38 @@
+let array = ["sunglasses", "cup", "two", "three", "four", "five", "six", "seven"];
+let useditems=[];
+let index;
+let index2;
+
 function generateGrid() {
     const board = document.getElementById('grid');
-    for (i=0; i<30; i++) {
+    for (let i=0; i<15; i++) {
         const square = document.createElement('div')
         board.appendChild(square)
     }
     boxes = document.querySelectorAll('#grid div')
 }
 
-let array = ["sunglasses", "cup", ""];
+function addItems() {
+    index = Math.floor(Math.random()*15);
+    index2 = Math.floor(Math.random()*(array.length));
+    usedimages.push(array[index2]);
+    while (usedimages.contains(array[index2])) {   
+        index = Math.floor(Math.random()*30);
+        index2 = Math.floor(Math.random()*(array.length));
+    }
+    boxes[index].classList.add(array[index2]);
+}
+
+function addItems2() {
+    for (let n=0; n < 5; n++) {
+        index2 = Math.floor(Math.random()*(array.length));
+        while (useditems.includes(array[index2])) {
+            index2 = Math.floor(Math.random()*(array.length));
+        }
+        useditems.push(array[index2])
+        boxes[n].classList.add(array[index2])
+    }
+}
 
 generateGrid();
+addItems2();
